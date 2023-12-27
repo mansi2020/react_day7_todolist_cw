e8fe309925f7aee9869cc12428dba36d46b7cd9a
mansi2020 <sorathiyamansi11@gmail.com>
2023-12-27 22:02:09 +0530

    add functionality

---
+++ src/Component/Main/Main.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import "./Main.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Main = () => {
+
+  // toaster
+  const showSucessMessage = () => {
+    toast.success("Success Notification !", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
+  const showFailedMessage = () => {
+    toast.error("Please Provide Value !", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
+  const showDeletedMessage = () => {
+    toast.warning("Item Deleted !", {
+      position: toast.POSITION.TOP_LEFT,
+    });
+  };
+
+   // save data on local storage----------------------------->
+   let saveDataOnLocalStorage = (updateTask)=>{
+    localStorage.setItem("localStoreData",JSON.stringify(updateTask));
+  }
+
+  useEffect(()=>{
+    let savedTask = JSON.parse(localStorage.getItem("localStoreData"));
+    if(savedTask){
+      setAddTask(savedTask);
+    }
+  },[])
+
+
+
+  // take user type input------------------------------------>
+  let [taskText, setTaskText] = useState("");
+  function onChageTaskText(e) {
+    setTaskText(e.target.value);
+  }
+
+  // click on add item button----------------->
+  let [addTask, setAddTask] = useState([]);
+  function onClickAddItem() {
+    if (taskText.trim() != "") {
+      let newAddTask = [...addTask, { text: taskText, completed: false }];
+      setAddTask(newAddTask);
+      saveDataOnLocalStorage(newAddTask);
+      setTaskText("");
+      showSucessMessage();
+    }else{
+      showFailedMessage();
+    }
+    
+  }
+  // remove task from task list click on delet button-------------------------------------------------->
+  function onClickRemoveTask(index) {
+    let updatedTask = [...addTask];
+    let filterTaskList = updatedTask.filter((task, taskIdx) => {
+      return index != taskIdx;
+    });
+    saveDataOnLocalStorage(filterTaskList);
+    setAddTask(filterTaskList);
+    showDeletedMessage();
+  }
+
+  // give line through in completed task------------------------------------------------------->
+  function onChangeTaskCompletion(index) {
+    let updatedTask = addTask.map((task, taskIdx) =>
+      index == taskIdx ? { ...task, completed: !task.completed } : task
+    );
+    setAddTask(updatedTask);
+    saveDataOnLocalStorage(updatedTask);
+  }
+
+ return (
+    <>
+      <div className="main_container">
+        <h1 className="main_heading">Grocery Bud</h1>
+
+        {/* add item input and button */}
+        <div className="main_add_item">
+          <input type="text" onChange={onChageTaskText} value={taskText} />
+          <button onClick={onClickAddItem} >Add Item</button>
+        </div>
+
+        {/* show task div */}
+        <ul>
+          {addTask.map((item, index) => (
+            <li className="main_task" key={index}>
+              <div className="main_content">
+                <input
+                  type="checkbox"
+                  onChange={() => onChangeTaskCompletion(index)}
+                  checked={item.completed}
+                />
+                <label
+                  className="task_para"
+                  style={{
+                    textDecoration:
+                      item.completed == true ? "line-through" : "none",
+                  }}
+                >
+                  {item.text}
+                </label>
+              </div>
+              <button onClick={() => onClickRemoveTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
+export default Main;
